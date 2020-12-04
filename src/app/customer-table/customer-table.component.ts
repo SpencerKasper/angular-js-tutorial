6604d46a184d880897e0428afd18deda05873c27
Spencer Kasper <spencer.kasper@gmail.com>
2020-12-04 13:05:08 -0600

    * Explore data binding from parent to child component

--- src/app/customer-table/customer-table.component.ts
+++ src/app/customer-table/customer-table.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-customer-table',
   templateUrl: './customer-table.component.html',
   styleUrls: ['./customer-table.component.scss']
 })
-export class CustomerTableComponent implements OnInit {
+export class CustomerTableComponent implements OnChanges, OnInit {
+  @Input() quantity: number | string;
 
-  constructor() { }
+  constructor() {
+    this.quantity = 7;
+    console.log('quantity in constructor: ', this.quantity);
+  }
 
   ngOnInit(): void {
+    this.quantity = 8;
+    console.log('quantity in onInit: ', this.quantity);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.quantity = 9;
+    console.log('quantity in onChanges: ', this.quantity);
   }
 
 }
