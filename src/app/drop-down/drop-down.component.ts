5942bee9f26df33e76125437b60366f3bed8f34a
Spencer Kasper <spencer.kasper@gmail.com>
2020-12-04 14:13:01 -0600

    * Add items to the dropdown and print the selected value on click

--- src/app/drop-down/drop-down.component.ts
+++ src/app/drop-down/drop-down.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./drop-down.component.scss']
 })
 export class DropDownComponent {
-
+  dropDownItems: Array<string | number> = [1,2,3,4,5,6,7,8,9];
   @Input() isOpen: boolean;
 
   constructor() {
@@ -19,4 +19,8 @@ export class DropDownComponent {
     console.log('Toggle after: ', this.isOpen);
   }
 
+  public handleItemSelect(event: MouseEvent, item: string | number): void {
+    console.log('item: ', item);
+    this.toggle();
+  }
 }
